a0686b754df5d39a9e8429ac7c277b14f440b65b
Chris Vandevelde <chris.vandevelde@uwaterloo.ca>
2013-06-25 20:47:18 -0400

    Modified script to randomly select from images available and avoid libraries.

--- index.php
+++ index.php
@@ -1,43 +1,58 @@
-<!doctype html>
-<head>
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
+<?php
+	$accept = strtolower(str_replace(' ', '', $_SERVER['HTTP_ACCEPT']));
+    $accept = explode(',', $accept);
+
+    $dir = "img";
+	$dh  = opendir($dir);
+	$files = array();
+	while (false !== ($filename = readdir($dh))) {
+		if (!in_array($filename, array('.', '..'))) {
+	    	$files[] = $filename;
+	    }
+	}
+
+	$random_image = $files[array_rand($files)];
+
+    if (in_array('application/json', $accept)) {
+    	print json_encode(array('image' => $random_image));
+    } else {
+    	?><!doctype html>
+		<html>
+			<head>
+				<style>
+					body {
+						padding: 0;
+						margin: 0;
+					}
+
+					div.stuff {
+						position: fixed;
+						left: 0;
+						top: 0;
+						width: 100%;
+						height: 100%;
+						min-height: 100%;
+						background-image: url('img/<?= $random_image ?>');
+						background-repeat: no-repeat;
+						background-attachment: fixed;
+						background-position: center; 
+						background-size: cover;
+					}
+				</style>
+				<script src="js/jquery-2.0.2.min.js"></script>
+			</head>
+			<body>
+				<div class="stuff">&nbsp;</div>
+				<script>
+					$(function() {
+						setInterval(function() {
+							$.getJSON('http://www.eng.uwaterloo.ca/~englcdtv/', function(data) {
+								$('div.stuff').css('background-image', 'url(img/' + data['image'] + ')');
+							});
+						}, 1 * 1000);
+					});
+				</script>
+			</body>
+		</html><?php
     }
-  </style>
-
-  <link rel="stylesheet" href="css/superslides.css">
-
-  <script src="js/jquery-2.0.2.min.js"></script>
-  <script src="js/jquery.superslides.min.js"></script>
-  <script src="js/jquery.animate-enhanced.min.js"></script>
-
-  <script>
-    $(function(){
-      $("#slides").superslides({
-        play: 5000, // Milliseconds for delay
-        slide_speed: 'normal',
-        slide_easing: 'easeInOutCubic',
-        pagination: false,
-        scrollable: false, // Allow scrollable content inside slide
-      });
-      $("#slides").superslides("start");
-    });
-  </script>
-</head>
-<body>
-  <div id="slides">
-  <ul class="slides-container">
-    <?php
-    $directory = 'img';
-    $images_directory = array_diff(scandir($directory), array('..', '.'));
-    foreach($images_directory as $image) {
-      ?><li>
-        <img src="img/<?= $image ?>" alt="">
-      </li><?php
-    }
-    ?>
-  </ul>
-</div>
-</body>
+?>
